e15364177232ef0f9aac0eaefd1c958eebcfb79f
Athan <jonpuray@gmail.com>
2025-01-22 10:49:35 +0800

    creation of main page

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 with st.sidebar:
 
-    st.text_input(label="USERNAME", key="_user")
-    st.text_input(label="PASSWORD", key="_pass")
-    st.button(label="SIGN IN")
+    with st.container():
+        st.text_input(label="USERNAME", key="_user")
+        st.text_input(label="PASSWORD", key="_pass")
+        st.button(label="SIGN IN")
 
 
     
