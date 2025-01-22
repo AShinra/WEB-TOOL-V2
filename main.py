af41c064f1885875dc3332302746209e0f92bd36
Athan <jonpuray@gmail.com>
2025-01-22 10:51:24 +0800

    creation of main page

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 with st.sidebar:
 
+    st.header("MMI Web Tool V2.0")
+
     with st.container(border=True):
         st.text_input(label="USERNAME", key="_user")
         st.text_input(label="PASSWORD", key="_pass")
